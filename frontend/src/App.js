6d52e2a315ce529df7dccc8f8c816293fe95910b
Therobo77 <anshusinha8873@gmail.com>
2023-02-22 22:29:12 +0530

    By EOD

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import MainRoute from './pages/MainRoute';
 import { Button } from '@chakra-ui/react';
+import Username from './login/Username';
 
 function App() {
   return (
     <div className="App">
      <MainRoute/>
-     <Button>Hello World</Button>
+     <Username/>
     </div>
   );
 }
