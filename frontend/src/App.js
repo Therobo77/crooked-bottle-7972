0c542f230b5ef0f58d0e357092a4374e2e19e734
Therobo77 <anshusinha8873@gmail.com>
2023-02-22 12:27:56 +0530

    chakra-added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import MainRoute from './pages/MainRoute';
+import { Button } from '@chakra-ui/react';
 
 function App() {
   return (
     <div className="App">
      <MainRoute/>
+     <Button>Hello World</Button>
     </div>
   );
 }
