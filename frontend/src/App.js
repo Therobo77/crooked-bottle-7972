152e629e6c3cf6c4ee4b91ba37f39587a6a23389
Therobo77 <anshusinha8873@gmail.com>
2023-02-23 14:11:38 +0530

    (fp04_059_day-3)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,13 @@
-
-import logo from './logo.svg';
-import './App.css';
-import MainRoute from './pages/MainRoute';
-import Username from './login/Username';
-
-
+import logo from "./logo.svg";
+import "./App.css";
+import MainRoute from "./pages/MainRoute";
+import Username from "./login/Username";
 
 function App() {
   return (
     <div className="App">
-
-
-     <MainRoute/>
-     <Username/>
+      <MainRoute />
+      <Username />
     </div>
   );
 }
