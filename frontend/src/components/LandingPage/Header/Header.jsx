b79e67e7012b31e2bde32006b9f8b0722868ace8
rohit715131 <rohit715131@gmail.com>
2023-02-26 23:37:13 +0530

    changes

--- frontend/src/components/LandingPage/Header/Header.jsx
+++ frontend/src/components/LandingPage/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "./Header.css";
 import logo from "../../../assets/logo.png";
-import SearchIcon from '@mui/icons-material/Search';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import SearchIcon from "@mui/icons-material/Search";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -12,23 +13,27 @@ const Header = () => {
         <img src={logo} alt="logo"></img>
       </div>
       <div className="header_search">
-        <input type="text" placeholder="Search for products, brands and more"/>
-        <SearchIcon/>
+        <input type="text" placeholder="Search for products, brands and more" />
+        <SearchIcon />
       </div>
-     
+
       <div className="header_login">
         <button>Log in</button>
       </div>
       <div className="header_admin">
-        <p>Become A Seller</p>
+        <Link to="/admin">
+          <p>Become A Seller</p>
+        </Link>
       </div>
       <div className="header_more">
         <p>More</p>
-        <ExpandMoreIcon/>
+        <ExpandMoreIcon />
       </div>
       <div className="header_cart">
-      <ShoppingCartIcon/>
-        <p>cart</p>
+        <Link to="/cart">
+          <ShoppingCartIcon />
+          <p>cart</p>
+        </Link>
       </div>
     </div>
   );
